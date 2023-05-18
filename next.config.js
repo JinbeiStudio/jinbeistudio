9ad29a2d2889bf185e6fef34935bcb021a1d05b5
JinbeiStudio <julien.gabriel@me.com>
2023-05-18 11:25:52 +0200

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  output: "export",
+  basePath: "/jinbeistudio",
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig

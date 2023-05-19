24f904bfee7dd9bbf88b0104821ef7f8d0484d8a
JinbeiStudio <julien.gabriel@me.com>
2023-05-19 23:46:47 +0200

    export script

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  output: "export",
   basePath: "/jinbeistudio",
+  assetPrefix: isProd ? "/jinbeistudio/" : "",
   experimental: {
     appDir: true,
   },

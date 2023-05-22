35f0a8f7bbc017e5330c1b0fe64cadcb9de7ad2b
JinbeiStudio <julien.gabriel@me.com>
2023-05-22 18:13:23 +0200

    seo

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,11 @@ import "slick-carousel/slick/slick-theme.css";
 const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Jinbei Studio",
-  description: "Jinbei Studio par Julien Gabriel - Développeur Fullstack",
+  title: "Jinbei Studio | Julien Gabriel | Développeur Fullstack à Lannion",
+  description: "Créé par Julien Gabriel, Jinbei Studio est une entreprise de création de sites internet et d'applications web et mobiles à Lannion en Bretagne. Jinbei Studio est là pour vous conseiller et vous apporter les solutions adaptées à vos différents projets.",
+  creator: 'Julien Gabriel',
+  publisher: 'Julien Gabriel',
+  keywords: ['Jinbei Studio', 'Julien Gabriel', 'Développeur Fullstack', "Développement Application Web", "Développement Application Mobile", "Création Site Web"],
 };
 
 export default function RootLayout({ children }) {

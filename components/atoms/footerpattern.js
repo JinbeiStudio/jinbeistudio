97bee65c3b21d0d874e11057426a48c2be9bb2aa
JinbeiStudio <julien.gabriel@me.com>
2023-08-07 22:20:02 +0200

    fix footer pattern mobile v3

--- components/atoms/footerpattern.js
+++ components/atoms/footerpattern.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function FooterPattern() {
-  return <div className="h-[50px]" style={{ backgroundImage: 'url(/images/footer-pattern.svg)', backgroundSize: 'cover' }} />;
+  return <div className="max-sm:h-[35px] h-[50px]" style={{ backgroundImage: 'url(/images/footer-pattern.svg)' }} />;
 }
